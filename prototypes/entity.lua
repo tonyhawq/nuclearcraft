7d9661292ddd251c01c3b799c29a336270b63806
tonyhawq <larrylarsonbest@gmail.com>
2025-02-11 22:36:43 -0800

    rotate no

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -2,7 +2,7 @@ data:extend({
     {
         type = "constant-combinator",
         name = "fuel-rod",
-        flags = {"placeable-neutral", "player-creation"},
+        flags = {"placeable-neutral", "player-creation", "not-rotateable"},
         icons = {
             {
                 icon = "__control-your-rods__/graphics/entity/green.png",
